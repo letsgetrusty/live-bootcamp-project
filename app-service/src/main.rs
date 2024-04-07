def3cfa7d6d4de212a869bfe304e081a4365c8b4
letsgetrusty <bogdan@letsgetrusty.com>
2024-04-07 10:42:41 +0200

    init

---
+++ app-service/src/main.rs
@@ -0,0 +1,88 @@
+use std::env;
+
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+    Json, Router,
+};
+use axum_extra::extract::CookieJar;
+use serde::Serialize;
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .nest_service("/assets", ServeDir::new("assets"))
+        .route("/", get(root))
+        .route("/protected", get(protected));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    login_link: String,
+    logout_link: String,
+}
+
+async fn root() -> impl IntoResponse {
+    let mut address = env::var("AUTH_SERVICE_IP").unwrap_or("localhost".to_owned());
+    if address.is_empty() {
+        address = "localhost".to_owned();
+    }
+    let login_link = format!("http://{}:3000", address);
+    let logout_link = format!("http://{}:3000/logout", address);
+
+    let template = IndexTemplate {
+        login_link,
+        logout_link,
+    };
+    Html(template.render().unwrap())
+}
+
+async fn protected(jar: CookieJar) -> impl IntoResponse {
+    let jwt_cookie = match jar.get("jwt") {
+        Some(cookie) => cookie,
+        None => {
+            return StatusCode::UNAUTHORIZED.into_response();
+        }
+    };
+
+    let api_client = reqwest::Client::builder().build().unwrap();
+
+    let verify_token_body = serde_json::json!({
+        "token": &jwt_cookie.value(),
+    });
+
+    let auth_hostname = env::var("AUTH_SERVICE_HOST_NAME").unwrap_or("0.0.0.0".to_owned());
+    let url = format!("http://{}:3000/verify-token", auth_hostname);
+
+    let response = match api_client.post(&url).json(&verify_token_body).send().await {
+        Ok(response) => response,
+        Err(_) => {
+            return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+        }
+    };
+
+    match response.status() {
+        reqwest::StatusCode::UNAUTHORIZED | reqwest::StatusCode::BAD_REQUEST => {
+            StatusCode::UNAUTHORIZED.into_response()
+        }
+        reqwest::StatusCode::OK => Json(ProtectedRouteResponse {
+            img_url: "https://i.ibb.co/YP90j68/Light-Live-Bootcamp-Certificate.png".to_owned(),
+        })
+        .into_response(),
+        _ => StatusCode::INTERNAL_SERVER_ERROR.into_response(),
+    }
+}
+
+#[derive(Serialize)]
+pub struct ProtectedRouteResponse {
+    pub img_url: String,
+}
---
+++ auth-service/src/main.rs
@@ -0,0 +1,22 @@
+use axum::{response::Html, routing::get, Router};
+use tower_http::services::ServeDir;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .nest_service("/", ServeDir::new("assets"))
+        .route("/hello", get(hello_handler));
+
+    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
+    // This is needed for Docker to work, which we will add later on.
+    // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_handler() -> Html<&'static str> {
+    // TODO: Update this to a custom message!
+    Html("<h1>Hello, World!</h1>")
+}
