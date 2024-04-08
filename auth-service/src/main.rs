95201bcb409ff134c59e2f5cf32aeef2d70f7c1a
letsgetrusty <bogdan@letsgetrusty.com>
2024-04-08 14:13:31 +0200

    Completed sprint 1 tasks

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -17,6 +17,5 @@ async fn main() {
 }
 
 async fn hello_handler() -> Html<&'static str> {
-    // TODO: Update this to a custom message!
-    Html("<h1>Hello, World!</h1>")
+    Html("<h1>Hello, World! My name is Bogdan!</h1>")
 }
