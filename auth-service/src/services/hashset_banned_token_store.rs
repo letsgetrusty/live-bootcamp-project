91ee8cda05e148b3bc8ab65ec0374b6ef23279c2
letsgetrusty <bogdan@letsgetrusty.com>
2024-04-22 11:40:12 +0200

    Completed sprint 4 tasks

---
+++ auth-service/src/services/hashset_banned_token_store.rs
@@ -0,0 +1,46 @@
+use std::collections::HashSet;
+
+use crate::domain::data_stores::{BannedTokenStore, BannedTokenStoreError};
+
+#[derive(Default)]
+pub struct HashsetBannedTokenStore {
+    tokens: HashSet<String>,
+}
+
+#[async_trait::async_trait]
+impl BannedTokenStore for HashsetBannedTokenStore {
+    async fn add_token(&mut self, token: String) -> Result<(), BannedTokenStoreError> {
+        self.tokens.insert(token);
+        Ok(())
+    }
+
+    async fn contains_token(&self, token: &str) -> Result<bool, BannedTokenStoreError> {
+        Ok(self.tokens.contains(token))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn test_add_token() {
+        let mut store = HashsetBannedTokenStore::default();
+        let token = "test_token".to_owned();
+
+        let result = store.add_token(token.clone()).await;
+
+        assert!(result.is_ok());
+        assert!(store.tokens.contains(&token));
+    }
+
+    #[tokio::test]
+    async fn test_contains_token() {
+        let mut store = HashsetBannedTokenStore::default();
+        let token = "test_token".to_owned();
+        store.tokens.insert(token.clone());
+
+        let result = store.contains_token(&token).await;
+
+        assert!(result.unwrap());
+    }
+}
