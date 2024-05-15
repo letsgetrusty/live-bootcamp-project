15c21583410a50d88ef52c66629fa66fc0736f4a
letsgetrusty <bogdan@letsgetrusty.com>
2024-05-15 20:54:04 +0200

    Completed sprint 6 tasks

--- auth-service/tests/api/logout.rs
+++ auth-service/tests/api/logout.rs
@@ -36,36 +36,6 @@ async fn should_return_200_if_valid_jwt_cookie() {
 
     let token = auth_cookie.value();
 
-    let random_email = get_random_email();
-
-    let signup_body = serde_json::json!({
-        "email": random_email,
-        "password": "password123",
-        "requires2FA": false
-    });
-
-    let response = app.post_signup(&signup_body).await;
-
-    assert_eq!(response.status().as_u16(), 201);
-
-    let login_body = serde_json::json!({
-        "email": random_email,
-        "password": "password123",
-    });
-
-    let response = app.post_login(&login_body).await;
-
-    assert_eq!(response.status().as_u16(), 200);
-
-    let auth_cookie = response
-        .cookies()
-        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
-        .expect("No auth cookie found");
-
-    assert!(!auth_cookie.value().is_empty());
-
-    let token = auth_cookie.value();
-
     let response = app.post_logout().await;
 
     assert_eq!(response.status().as_u16(), 200);
