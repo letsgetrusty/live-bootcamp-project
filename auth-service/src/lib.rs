fc786b0960390ee049094ba4f045ba9652e3a398
mpdevilleres <mpdevilleres@gmail.com>
2024-10-09 21:31:29 +0400

    [Sprint 1][Task 4] * refactored Application to lib and keep main as a wrapper binary

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,38 @@
+use std::error::Error;
+use axum::{serve::Serve, response::Html, routing::get, Router};
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        // Move the Router definition from `main.rs` to here.
+        // Also, remove the `hello` route.
+        // We don't need it at this point!
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/hello", get(hello_handler));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        // Create a new Application instance and return it
+        Ok(Self { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
+
+async fn hello_handler() -> Html<&'static str> {
+    Html("<h1>🦀🐍🦀🐍</h1>")
+}
