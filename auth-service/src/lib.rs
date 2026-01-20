0e0eb008599ca3096a19476753e89483873f0f1b
Hamza <halgamoos@gmail.com>
2026-01-19 21:22:14 -0800

    refactor: structure the auth service for testing
    Completed tasks 1-3

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,33 @@
+use std::error::Error;
+
+use axum::serve::Serve;
+use axum::{Router};
+use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
+
+// This struct encapsulates our application-related logic.
+pub struct Application {
+    server: Serve<TcpListener, Router, Router>,
+    // address is exposed as a public field
+    // so we have access to it in tests.
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let assets_dir = ServeDir::new("assets");
+        let router = Router::new()
+            .fallback_service(assets_dir);
+
+        let listener = TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        Ok(Application { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,23 +1,10 @@
-use axum::{response::Html, routing::get, serve, Router};
-use tower_http::services::ServeDir;
+use auth_service::Application;
 
 #[tokio::main]
 async fn main() {
-    let assets_dir = ServeDir::new("assets");
-    let app = Router::new()
-        .fallback_service(assets_dir)
-        .route("/hello", get(hello_handler));
+    let app = Application::build("0.0.0.0:3000")
+        .await
+        .expect("Failed to build app");
 
-    // Here we are using ip 0.0.0.0 so the service is listening on all the configured network interfaces.
-    // This is needed for Docker to work, which we will add later on.
-    // See: https://stackoverflow.com/questions/39525820/docker-port-forwarding-not-working
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    println!("listening on {}", listener.local_addr().unwrap());
-
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn hello_handler() -> Html<&'static str> {
-    // TODO: Update this to a custom message!
-    Html("<h1>Hello, World!</h1>")
+    app.run().await.expect("Failed to run app");
 }
