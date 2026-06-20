f020b501e1d5edd14bd67d80e6d6f7fa7e0e35b5
EleisonC <ckalule7@gmail.com>
2026-06-20 13:34:14 -0400

    use idiomatic error propagation in handlers

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -13,30 +13,23 @@ pub async fn login(
     jar: CookieJar,
     Json(request): Json<LoginRequest>,
 ) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
-    match HashedPassword::parse(request.password.clone()).await {
-        Ok(password) => password,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
-    };
+    HashedPassword::parse(request.password.clone())
+        .await
+        .map_err(|_| AuthAPIError::InvalidCredentials)?;
 
-    let email = match Email::parse(request.email) {
-        Ok(email) => email,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
-    };
+    let email = Email::parse(request.email).map_err(|_| AuthAPIError::InvalidCredentials)?;
 
     let user_store = &state.user_store.read().await;
 
-    if user_store
+    user_store
         .validate_user(&email, &request.password)
         .await
-        .is_err()
-    {
-        return Err(AuthAPIError::IncorrectCredentials);
-    }
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
 
-    let user = match user_store.get_user(&email).await {
-        Ok(user) => user,
-        Err(_) => return Err(AuthAPIError::IncorrectCredentials),
-    };
+    let user = user_store
+        .get_user(&email)
+        .await
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
 
     match user.requires_2fa {
         true => handle_2fa(&user.email, &state, jar).await,
@@ -52,25 +45,19 @@ async fn handle_2fa(
     let login_attempt_id = LoginAttemptId::default();
     let two_fa_code = TwoFACode::default();
 
-    if state
+    state
         .two_fa_code_store
         .write()
         .await
         .add_code(email.clone(), login_attempt_id.clone(), two_fa_code.clone())
         .await
-        .is_err()
-    {
-        return Err(AuthAPIError::UnexpectedError);
-    }
+        .map_err(|_| AuthAPIError::UnexpectedError)?;
 
-    if state
+    state
         .email_client
         .send_email(email, "2FA Code", two_fa_code.as_ref())
         .await
-        .is_err()
-    {
-        return Err(AuthAPIError::UnexpectedError);
-    }
+        .map_err(|_| AuthAPIError::UnexpectedError)?;
 
     let response = Json(LoginResponse::TwoFactorAuth(TwoFactorAuthResponse {
         message: "2FA required".to_owned(),
@@ -84,10 +71,7 @@ async fn handle_no_2fa(
     email: &Email,
     jar: CookieJar,
 ) -> Result<(CookieJar, (StatusCode, Json<LoginResponse>)), AuthAPIError> {
-    let auth_cookie = match generate_auth_cookie(email) {
-        Ok(cookie) => cookie,
-        Err(_) => return Err(AuthAPIError::UnexpectedError),
-    };
+    let auth_cookie = generate_auth_cookie(email).map_err(|_| AuthAPIError::UnexpectedError)?;
 
     let updated_jar = jar.add(auth_cookie);
 
--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -13,40 +13,32 @@ pub async fn verify_2fa(
     jar: CookieJar,
     Json(request): Json<Verify2FARequest>,
 ) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
-    let email = match Email::parse(request.email.clone()) {
-        Ok(email) => email,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
-    };
+    let email =
+        Email::parse(request.email.clone()).map_err(|_| AuthAPIError::InvalidCredentials)?;
 
-    let login_attempt_id = match LoginAttemptId::parse(request.login_attempt_id.clone()) {
-        Ok(login_attempt_id) => login_attempt_id,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
-    };
+    let login_attempt_id = LoginAttemptId::parse(request.login_attempt_id.clone())
+        .map_err(|_| AuthAPIError::InvalidCredentials)?;
 
-    let two_fa_code = match TwoFACode::parse(request.two_fa_code) {
-        Ok(two_fa_code) => two_fa_code,
-        Err(_) => return Err(AuthAPIError::InvalidCredentials),
-    };
+    let two_fa_code =
+        TwoFACode::parse(request.two_fa_code).map_err(|_| AuthAPIError::InvalidCredentials)?;
 
     let mut two_fa_code_store = state.two_fa_code_store.write().await;
 
-    let code_tuple = match two_fa_code_store.get_code(&email).await {
-        Ok(code_tuple) => code_tuple,
-        Err(_) => return Err(AuthAPIError::IncorrectCredentials),
-    };
+    let code_tuple = two_fa_code_store
+        .get_code(&email)
+        .await
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
 
     if !code_tuple.0.eq(&login_attempt_id) || !code_tuple.1.eq(&two_fa_code) {
         return Err(AuthAPIError::IncorrectCredentials);
     }
 
-    if two_fa_code_store.remove_code(&email).await.is_err() {
-        return Err(AuthAPIError::UnexpectedError);
-    }
+    two_fa_code_store
+        .remove_code(&email)
+        .await
+        .map_err(|_| AuthAPIError::UnexpectedError)?;
 
-    let cookie = match generate_auth_cookie(&email) {
-        Ok(cookie) => cookie,
-        Err(_) => return Err(AuthAPIError::UnexpectedError),
-    };
+    let cookie = generate_auth_cookie(&email).map_err(|_| AuthAPIError::UnexpectedError)?;
 
     let updated_jar = jar.add(cookie);
 
