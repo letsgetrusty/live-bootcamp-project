f1d1682d641e3bb7a9c106448bebddf88964bb0a
christopher k <ckalule7@gmail.com>
2026-07-13 08:25:49 -0400

    Sprint 6: update alpine version and verify_2fa, logout & login return type (#80)
    * update alpine version and verify_2fa, logout & login return type
    
    * use idiomatic error propagation in handlers

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -14,31 +14,24 @@ pub async fn login(
     State(state): State<AppState>,
     jar: CookieJar,
     Json(request): Json<LoginRequest>,
-) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
-    match HashedPassword::parse(request.password.clone()).await {
-        Ok(password) => password,
-        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
-    };
+) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
+    HashedPassword::parse(request.password.clone())
+        .await
+        .map_err(|_| AuthAPIError::InvalidCredentials)?;
 
-    let email = match Email::parse(request.email) {
-        Ok(email) => email,
-        Err(_) => return (jar, Err(AuthAPIError::InvalidCredentials)),
-    };
+    let email = Email::parse(request.email).map_err(|_| AuthAPIError::InvalidCredentials)?;
 
     let user_store = &state.user_store.read().await;
 
-    if user_store
+    user_store
         .validate_user(&email, &request.password)
         .await
-        .is_err()
-    {
-        return (jar, Err(AuthAPIError::IncorrectCredentials));
-    }
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
 
-    let user = match user_store.get_user(&email).await {
-        Ok(user) => user,
-        Err(_) => return (jar, Err(AuthAPIError::IncorrectCredentials)),
-    };
+    let user = user_store
+        .get_user(&email)
+        .await
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
 
     match user.requires_2fa {
         true => handle_2fa(&user.email, &state, jar).await,
@@ -51,58 +44,45 @@ async fn handle_2fa(
     email: &Email,
     state: &AppState,
     jar: CookieJar,
-) -> (
-    CookieJar,
-    Result<(StatusCode, Json<LoginResponse>), AuthAPIError>,
-) {
+) -> Result<(CookieJar, (StatusCode, Json<LoginResponse>)), AuthAPIError> {
     let login_attempt_id = LoginAttemptId::default();
     let two_fa_code = TwoFACode::default();
 
-    if let Err(e) = state
+    state
         .two_fa_code_store
         .write()
         .await
         .add_code(email.clone(), login_attempt_id.clone(), two_fa_code.clone())
         .await
-    {
-        return (jar, Err(AuthAPIError::UnexpectedError(e.into())));
-    }
+        .map_err(|e| AuthAPIError::UnexpectedError(e.into()))?;
 
-    if let Err(e) = state
+    state
         .email_client
         .send_email(email, "2FA Code", two_fa_code.as_ref().expose_secret())
         .await
-    {
-        return (jar, Err(AuthAPIError::UnexpectedError(e)));
-    }
+        .map_err(|e| AuthAPIError::UnexpectedError(e.into()))?;
 
     let response = Json(LoginResponse::TwoFactorAuth(TwoFactorAuthResponse {
         message: "2FA required".to_owned(),
         login_attempt_id: login_attempt_id.as_ref().expose_secret().to_owned(),
     }));
 
-    (jar, Ok((StatusCode::PARTIAL_CONTENT, response)))
+    Ok((jar, (StatusCode::PARTIAL_CONTENT, response)))
 }
 
 #[tracing::instrument(name = "Handle non-2FA flow", skip_all)]
 async fn handle_no_2fa(
     email: &Email,
     jar: CookieJar,
-) -> (
-    CookieJar,
-    Result<(StatusCode, Json<LoginResponse>), AuthAPIError>,
-) {
-    let auth_cookie = match generate_auth_cookie(email) {
-        Ok(cookie) => cookie,
-        Err(e) => return (jar, Err(AuthAPIError::UnexpectedError(e))),
-    };
+) -> Result<(CookieJar, (StatusCode, Json<LoginResponse>)), AuthAPIError> {
+    let auth_cookie = generate_auth_cookie(email).map_err(|e| AuthAPIError::UnexpectedError(e))?;
 
     let updated_jar = jar.add(auth_cookie);
 
-    (
+    Ok((
         updated_jar,
-        Ok((StatusCode::OK, Json(LoginResponse::RegularAuth))),
-    )
+        (StatusCode::OK, Json(LoginResponse::RegularAuth)),
+    ))
 }
 
 #[derive(Deserialize)]
--- auth-service/src/routes/logout.rs
+++ auth-service/src/routes/logout.rs
@@ -12,32 +12,26 @@ use crate::{
 pub async fn logout(
     State(state): State<AppState>,
     jar: CookieJar,
-) -> (CookieJar, Result<impl IntoResponse, AuthAPIError>) {
-    let cookie = match jar.get(JWT_COOKIE_NAME) {
-        Some(cookie) => cookie,
-        None => return (jar, Err(AuthAPIError::MissingToken)),
-    };
+) -> Result<(CookieJar, impl IntoResponse), AuthAPIError> {
+    let cookie = jar.get(JWT_COOKIE_NAME).ok_or(AuthAPIError::MissingToken)?;
 
     // Validate token
     let token = SecretString::new(cookie.value().to_owned().into_boxed_str());
-    let _ = match validate_token(&token, state.banned_token_store.clone()).await {
-        Ok(claims) => claims,
-        Err(_) => return (jar, Err(AuthAPIError::InvalidToken)),
-    };
+    validate_token(&token, state.banned_token_store.clone())
+        .await
+        .map_err(|_| AuthAPIError::InvalidToken)?;
 
     // Add token to banned list
-    if let Err(e) = state
+    state
         .banned_token_store
         .write()
         .await
         .add_token(token.to_owned())
         .await
-    {
-        return (jar, Err(AuthAPIError::UnexpectedError(e.into())));
-    }
+        .map_err(|e| AuthAPIError::UnexpectedError(e.into()))?;
 
     // Remove jwt cookie
     let jar = jar.remove(cookie::Cookie::from(JWT_COOKIE_NAME));
 
-    (jar, Ok(StatusCode::OK))
+    Ok((jar, StatusCode::OK))
 }
